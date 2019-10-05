9c08b731efc66abfbd3e1920b4f3f03d638e5d13
D3stros <patrick.fallmann@hotmail.com>
2019-10-05 23:48:41 +0200

    Implemented local storage for saving, deleting and updating logs

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -44,7 +44,16 @@ export class LogService {
   }
 
   getLogs(): Observable<Log[]> {
-    return of(this.logs);
+    if (localStorage.getItem("logs") === null) {
+      this.logs = [];
+    } else {
+      this.logs = JSON.parse(localStorage.getItem("logs"));
+    }
+    return of(
+      this.logs.sort((a, b) => {
+        return (b.date = a.date);
+      })
+    );
   }
 
   setFormLog(log: Log) {
@@ -53,6 +62,9 @@ export class LogService {
 
   addLog(log: Log) {
     this.logs.unshift(log);
+
+    // Add to local storage
+    localStorage.setItem("logs", JSON.stringify(this.logs));
   }
 
   updateLog(log) {
@@ -62,6 +74,9 @@ export class LogService {
       }
     });
     this.logs.unshift(log);
+
+    // Update local storage
+    localStorage.setItem("logs", JSON.stringify(this.logs));
   }
 
   deleteLog(log) {
@@ -70,6 +85,9 @@ export class LogService {
         this.logs.splice(index, 1);
       }
     });
+
+    // Delete from  local storage
+    localStorage.setItem("logs", JSON.stringify(this.logs));
   }
 
   clearState() {
